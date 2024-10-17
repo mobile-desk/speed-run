ccaa5f580370d3bfa9e44da28832b8a5506280bf
Mobile Desk <63956765+mobile-desk@users.noreply.github.com>
2024-10-17 15:07:17 +0100

    Create arithmetic.py

---
+++ Arithmetic-Operators/arithmetic.py
@@ -0,0 +1,15 @@
+def main():
+    A = int(input())
+    B = int(input())
+    # Print seven lines as described above
+    print(f"{A+B}")
+    print(f"{A-B}")
+    print(f"{A*B}")
+    print(f"{A//B}")
+    print(f"{A/B}")
+    print(f"{A%B}")
+    print(f"{A**B}")
+    return 0
+
+if __name__ == '__main__':
+    main()
